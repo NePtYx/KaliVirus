c83c692fdd880924dcb44d277cf44ba41674795e
NePtYx <44326394+NePtYx@users.noreply.github.com>
2018-10-20 23:55:06 -0300

    Create Fall.sh

---
+++ Fall.sh
@@ -0,0 +1,16 @@
+echo "Welcome to fall"
+echo " ===  ==  =  = "
+echo "=    =  = =  = "
+echo "==   ==== =  = "
+echo "=    =  =  =  ="
+echo "==============="
+echo "By NePtYx======"
+echo "==============="
+
+echo "¿Desea generar un virus spam para Linux?"
+select yn in "Si" "No"; do
+    case $yn in
+        Si ) echo "Generado:; break;;
+        No ) echo "Ok bro Bye"; exit;;
+    esac
+done
