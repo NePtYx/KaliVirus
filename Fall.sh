0101dba220479f916a138cfce9cfc0b05f4484ac
NePtYx <44326394+NePtYx@users.noreply.github.com>
2018-10-21 00:01:21 -0300

    Update Fall.sh

--- Fall.sh
+++ Fall.sh
@@ -10,7 +10,7 @@ echo "==============="
 echo "¿Desea generar un virus spam para Linux?"
 select yn in "Si" "No"; do
     case $yn in
-        Si ) echo "Generado:; break;;
+        Si ) echo "Generado: https://github.com/NePtYx/Genered8294.git"; break;;
         No ) echo "Ok bro Bye"; exit;;
     esac
 done
